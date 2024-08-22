208b45a427237a94a3f2b6b662abffd844f8afac
Eric-Obeng <obengeric.3958@gmail.com>
2024-08-22 23:20:37 +0000

    counter effects updated

--- src/app/state/counter.effects.ts
+++ src/app/state/counter.effects.ts
@@ -3,26 +3,25 @@ import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { Store } from '@ngrx/store';
 import { tap, switchMap } from 'rxjs/operators';
 import { increment, decrement, reset, setCount } from './counter.actions';
-import { selectCurrentCount } from './counter.selectors';
+import { selectCounterState } from './counter.selectors';
+import { AppState } from '../interface/counterState.model';
 
 @Injectable()
 export class CounterEffects {
-
-  saveCountToLocalStorage$ = createEffect(() =>
-    this.actions$.pipe(
-      ofType(increment, decrement, reset, setCount),
-      switchMap(() => this.store.select(selectCurrentCount)),
-      tap(count => {
-        if (typeof window !== 'undefined') {
-          localStorage.setItem('counter', count.toString());
-        }
-      })
-    ),
+  // Effect to save the count to local storage
+  saveCountToLocalStorage$ = createEffect(
+    () =>
+      this.actions$.pipe(
+        ofType(increment, decrement, reset, setCount),
+        switchMap(() => this.store.select(selectCounterState)),
+        tap((count) => {
+          if (typeof window !== 'undefined') {
+            localStorage.setItem('counter', count.toString());
+          }
+        })
+      ),
     { dispatch: false }
   );
 
-  constructor(
-    private actions$: Actions,
-    private store: Store
-  ) {}
+  constructor(private actions$: Actions, private store: Store<AppState>) {}
 }
