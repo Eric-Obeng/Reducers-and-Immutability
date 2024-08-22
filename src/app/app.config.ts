ce846533562e595ca602ee05cae990533c45fe33
Eric-Obeng <obengeric.3958@gmail.com>
2024-08-22 23:21:26 +0000

    updated app.config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,18 +1,16 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
-import { provideStore, provideState } from '@ngrx/store';
-
+import { provideStore } from '@ngrx/store';
 import { counterReducer } from './state/counter.reducer';
 import { CounterEffects } from './state/counter.effects';
 import { provideEffects } from '@ngrx/effects';
+import { metaReducers } from './state/logger.meta-reducer';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideStore({counter: counterReducer}),
-    provideState({ name: 'counter', reducer: counterReducer }),
-    provideEffects([CounterEffects])
+    provideStore({ counter: counterReducer }, { metaReducers }), // This is the correct way to provide the store
+    provideEffects([CounterEffects]), // Provide your effects here
   ],
 };
